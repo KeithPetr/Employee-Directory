bab1ec31f860b15dcf19e8ce4bec9da3ed3488d1
KeithPetr <keith.petryshyn@gmail.com>
2023-03-06 14:01:31 -0500

    Centered card results
    Cards are now centered on the page as the results are filtered

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { employees } from "./employees.js";
 
 const employeeCardsEl = document.getElementById("employee-cards");
 const inputTextEl = document.getElementById("text-input");
+const selectEl = document.getElementById("team-selections");
 let startIndex = 0;
 let endIndex = 4;
 let loading = false;
@@ -31,38 +32,55 @@ function loadMoreEmployees() {
   loading = true;
   startIndex += 4;
   endIndex += 4;
+  filterResults(); // filter results based on the current search criteria
   const newEmployees = getEmployees(startIndex, endIndex);
   employeeCardsEl.innerHTML += newEmployees;
   loading = false;
 }
 
-// this function shows the results that match the letters that are
-// typed into the seach bar
-function showFilteredList(filteredEmployees) {
-  return filteredEmployees.map(employee => {
-    return `
-    <div class="card-container">
-        <div class="main-card-content">
-            <img class="profile-image" src="images/${employee.image}"></img>
-            <p class="employee-name">${employee.name}</p>
-            <p class="employee-title">${employee.title}</p>
-            ${employee.bio}
-        </div> 
-        <a href="${employee.social}"><img src="images/linkedin.svg" class="linkedIn"></a>
-    </div>
-    `;
-  }).join('')
+function filterResults() {
+  const team = selectEl.value;
+  const inputVal = inputTextEl.value.toLowerCase();
+  let filteredEmployees = filterDropdownList(team);
+  if (inputVal) {
+    filteredEmployees = filteredEmployees.filter((employee) => {
+      return employee.name.toLowerCase().includes(inputVal);
+    });
+  }
+  const html = filteredEmployees
+    .map((employee) => {
+      return `
+        <div class="card-container">
+            <div class="main-card-content">
+                <img class="profile-image" src="images/${employee.image}"></img>
+                <p class="employee-name">${employee.name}</p>
+                <p class="employee-title">${employee.title}</p>
+                ${employee.bio}
+            </div> 
+            <a href="${employee.social}"><img src="images/linkedin.svg" class="linkedIn"></a>
+        </div>
+        `;
+    })
+    .join("");
+  employeeCardsEl.innerHTML = html;
 }
 
-inputTextEl.addEventListener("input", (e) => {
-  const inputVal = e.target.value.toLowerCase();
-  const filteredEmployees = employees.filter((employee) => {
-    return employee.name.toLocaleLowerCase().includes(inputVal);
-  });
-  employeeCardsEl.innerHTML = showFilteredList(filteredEmployees);
-  console.log(filteredEmployees);
-  console.log(inputVal);
-});
+// this function searches through the employees data based on the
+// option that is selected from the dropdown list
+function filterDropdownList(team) {
+  let filteredEmployees = employees;
+  if (team !== "everyone") {
+    filteredEmployees = employees.filter((employee) => {
+      return employee.team === team;
+    });
+  }
+  return filteredEmployees;
+}
+
+// event listeners that look for the input of a character in the search bar
+// or a change in the dropdown menu selection
+inputTextEl.addEventListener("input", filterResults);
+selectEl.addEventListener("change", filterResults);
 
 window.addEventListener("scroll", function () {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
